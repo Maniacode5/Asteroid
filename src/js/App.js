3388b6e8741aef794e0e65a8901dc5bad435bd21
/2etr0 <5urf3r.mc@gmail.com>
2018-09-10 08:35:56 +0200

    jesaipa

--- src/js/App.js
+++ src/js/App.js
@@ -4,6 +4,12 @@ class App extends Component {
   render() {
     return (
       <div>
+        <svg>
+          <defs>
+            <path id="vaisseau" d="M 10,0 L 0,25 L 10,18 L 20,25 Z" style={{"stroke": "black", "fill": "white"}} />
+          </defs>
+          <use href="#vaisseau" x="10" y="10" />
+        </svg>
       </div>
     );
   }
