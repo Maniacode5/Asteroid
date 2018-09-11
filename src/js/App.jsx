5648a10f1ae27df56236c2b26a3c37f6349be52d
Tibomat <35692285+EchecEtMathis@users.noreply.github.com>
2018-09-11 15:51:10 +0200

    Deplacement (#10)
    * space move
    
    * teleport

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -5,60 +5,60 @@ import flatten from "lodash/flatten";
 import Vaisseau from './Vaisseau.jsx';
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+ return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 class App extends Component {
-    randomLines (points, r) {
-        return flatten(points.map((p, i, arr) => {
-            var next = arr[i+1];
+   randomLines (points, r) {
+       return flatten(points.map((p, i, arr) => {
+           var next = arr[i+1];
 
-            if (!next) {
-                next = arr[0];
-            }
+           if (!next) {
+               next = arr[0];
+           }
 
-            var nbSegments = getRandomInt(3, 4);
-            var segmentX = (next[0] - p[0]) / nbSegments;
-            var segmentY = (next[1] - p[1]) / nbSegments;
-            var newPoints = []
+           var nbSegments = getRandomInt(3, 4);
+           var segmentX = (next[0] - p[0]) / nbSegments;
+           var segmentY = (next[1] - p[1]) / nbSegments;
+           var newPoints = []
 
-            for (var j = 0; j <= nbSegments; j++) {
-                var coord = [
-                    getRandomInt((p[0] + (segmentX * j)) - r, (p[0] + (segmentX * j)) + r),
-                    getRandomInt((p[1] + (segmentY * j)) - r, (p[1] + (segmentY * j)) + r),
-                ]
-                newPoints.push(coord);
-            }
+           for (var j = 0; j <= nbSegments; j++) {
+               var coord = [
+                   getRandomInt((p[0] + (segmentX * j)) - r, (p[0] + (segmentX * j)) + r),
+                   getRandomInt((p[1] + (segmentY * j)) - r, (p[1] + (segmentY * j)) + r),
+               ]
+               newPoints.push(coord);
+           }
 
-            return newPoints;
-        }))
-    }
+           return newPoints;
+       }))
+   }
 
-    generateAsteroid() {
-        var points = [
-            [20, 0],
-            [40, 0],
-            [60, 20],
-            [60, 40],
-            [40, 60],
-            [20, 60],
-            [0, 40],
-            [0, 20]
-        ]
-        return <path d={`M ${this.randomLines(points, 4).map((p) => p.join(',')).join(" L ")} Z `} style={{"stroke": "white"}}/>
-    }
+   generateAsteroid() {
+       var points = [
+           [20, 0],
+           [40, 0],
+           [60, 20],
+           [60, 40],
+           [40, 60],
+           [20, 60],
+           [0, 40],
+           [0, 20]
+       ]
+       return <path d={`M ${this.randomLines(points, 4).map((p) => p.join(',')).join(" L ")} Z` } style={{"stroke": "white"}}/>
+   }
 
-    render() {
-        return (
-            <section>
-                <h1>ASTEROID</h1>
-                <svg className="Map">
-                    {this.generateAsteroid()}
-                    <Vaisseau />
-                </svg>
-            </section>
-        );
-    }
+   render() {
+       return (
+           <section>
+               <h1>ASTEROID</h1>
+               <svg id="Map" className="Map">
+                   {this.generateAsteroid()}
+                   <Vaisseau className="" move="z" turnLeft="q" turnRight="d" />
+               </svg>
+           </section>
+       );
+   }
 }
 
 export default App;
--- src/js/Vaisseau.jsx
+++ src/js/Vaisseau.jsx
@@ -1,29 +1,113 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import cx from "classnames";
+import Vector from "./Vector";
+import {positionLoop} from "./util"
+
+const DEFAULT_ROTATION = 10;
+const DEFAULT_SPEED = 50;
+const DEFAULT_ANGLE = -90;
 
 class Vaisseau extends Component {
-  state = {
-    mooving : null
-  }
-  componentWillMount() {
-    document.addEventListener("keyDown", (event) =>{
-      const { mooving } = this.state
-      switch (event.key) {
-        case this.props.upArrow:
-      }
-      console.log("fvzv")
-    })
-  }
+    state = {
+        trajectoire: undefined,
+        position: undefined,
+        mooving: false,
+        turning: false
+    };
 
-  render() {
-    return (
-      <svg>
-        <defs>
-            <path id="vaisseau" d="M 10,0 L 0,20 L 8,15 L 10,25 L 12,15 L 20,20 Z" style={{"stroke": "white"}} />
-        </defs>
-        <use href="#vaisseau" x="50" y="50"/>
-      </svg>
-    );
-  }
+    timer () {
+        setInterval(() => {
+            const { trajectoire, position, mooving, turning } = this.state;
+            const newState = {};
+
+            if (mooving) {
+                newState.position = positionLoop("Map", Vector.add(position, trajectoire));
+            }
+
+            if (turning) {
+                newState.trajectoire = new Vector(trajectoire.length, trajectoire.angle);
+
+                switch (turning) {
+                    case "left":
+                        newState.trajectoire.rotate(-DEFAULT_ROTATION);
+                        break;
+                    case "right":
+                        newState.trajectoire.rotate(DEFAULT_ROTATION);
+                        break;
+                }
+            }
+
+            this.setState(newState);
+        }, 10);
+    }
+
+    componentDidMount() {
+        const mapBounding = document.getElementById("Map").getBoundingClientRect();
+
+        this.setState({
+            position: Vector.fromCoordinates(
+                mapBounding.left + (mapBounding.width / 2),
+                mapBounding.top + (mapBounding.height / 2)
+            ),
+            trajectoire: new Vector(DEFAULT_SPEED, DEFAULT_ANGLE)
+        });
+
+        document.addEventListener('keydown', (event) => {
+            const newState = {};
+
+            switch (event.key){
+                case this.props.move:
+                    newState.mooving = true;
+                    break;
+
+                case this.props.turnLeft:
+                    newState.turning = "left";
+                    break;
+
+                case this.props.turnRight:
+                    newState.turning = "right";
+                    break;
+            }
+
+            this.setState(newState);
+        });
+
+        document.addEventListener('keyup', (event) => {
+            let { turning } = this.state;
+            const newState = {};
+
+            switch (event.key){
+                case this.props.move:
+                    newState.mooving = false;
+                    break;
+
+                case this.props.turnLeft:
+                    if (turning === "left") {
+                        newState.turning = false;
+                    }
+                    break;
+
+                case this.props.turnRight:
+                    if (turning === "right") {
+                        newState.turning = false;
+                    }
+                    break;
+            }
+
+            this.setState(newState);
+        });
+
+        this.timer();
+    }
+
+    render() {
+        const { position = { coordinates: { x: 0, y: 0 } }, trajectoire = {} } = this.state;
+        return (
+            <Fragment>
+                <path id="vaisseau" d="M 25,10 L 0,0 L 5,7.5 L -5,10 L 5,12.5 L 0,20 Z"  href="#vaisseau" className={cx("vaisseau", this.props.className)} transform={`rotate(${trajectoire.angle} ${position.coordinates.x + 10} ${position.coordinates.y+12.5}) translate(${position.coordinates.x} ${position.coordinates.y})`} stroke="white" />
+            </Fragment>
+        );
+    }
 }
 
 export default Vaisseau;
