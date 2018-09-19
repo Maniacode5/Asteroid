25e88f9b8aceac1c86ce8f8f0471c07e620703f0
/2etr0 <5urf3r.mc@gmail.com>
2018-09-19 13:40:59 +0200

    mrMLDEG

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -3,21 +3,68 @@ import React, { Component } from 'react';
 import Vaisseau from './Vaisseau.jsx';
 import Asteroid from './Asteroid.jsx';
 
+import { getRandomColor } from './util.js';
+
 class App extends Component {
-   render() {
-       return (
-           <section>
-               <h1>ASTEROID</h1>
-               <svg id="Map" className="Map">
-                   <Asteroid/>
-                   <Asteroid/>
-                   <Asteroid/>
-                   <Asteroid/>
-                   <Vaisseau className="" move="z" turnLeft="q" turnRight="d" />
-               </svg>
-           </section>
-       );
-   }
+  state = {
+    shooting: false,
+    bullets: []
+  };
+
+  componentWillMount() {
+    console.log(this.state);
+    window.addEventListener("keydown", (e) => {
+      var newState = {};
+
+      switch (e.keyCode) {
+        case 32:
+          if (this.state.bullets.length < 4) {
+            newState.shooting = true;
+          }
+          break;
+        default:
+          break;
+      }
+
+      this.setState(newState);
+    }, false);
+
+    window.addEventListener("keyup", (e) => {
+      var newState = {};
+
+      switch (e.keyCode) {
+        case 32:
+          newState.shooting = false;
+          break;
+        default:
+          break;
+      }
+
+      this.setState(newState);
+    }, false);
+  }
+
+  render() {
+    return (
+      <section>
+        <h1>ASTEROID</h1>
+        <svg id="Map" className="Map">$
+          <Asteroid />
+          <Asteroid />
+          <Asteroid />
+          <Vaisseau className="" move={40} turnLeft={37} turnRight={39} shoot={32} color={getRandomColor()} />
+          <ul>
+            {
+              this.state.bullets.map(() => {
+
+                return ;
+              })
+            }
+          </ul>
+        </svg>
+      </section>
+    );
+  }
 }
 
 export default App;
