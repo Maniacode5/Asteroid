c5e90661f3ebbfbf6bb11b9e4791c6b5cf72f86a
/2etr0 <5urf3r.mc@gmail.com>
2018-09-21 17:08:08 +0200

    home

--- src/js/ChooseColor.jsx
+++ src/js/ChooseColor.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import { getRandomColor } from './util';
+
 class ChooseColor extends Component {
     state= {
         displayed: 'color chooser'
@@ -10,39 +12,55 @@ class ChooseColor extends Component {
         const colors = [
             {
                 colId: '#ffffff',
-                colName: 'white'
+                colName: 'White'
             },
             {
                 colId: '#ff0022',
-                colName: 'red'
+                colName: 'Red'
             },
             {
                 colId: '#0009c5',
-                colName: 'blue'
+                colName: 'Blue'
             },
             {
                 colId: '#06c504',
-                colName: 'green'
+                colName: 'Green'
             },
             {
                 colId: '#8e048e',
-                colName: 'purple'
+                colName: 'Purple'
+            },
+            {
+                colId: null,
+                colName: 'Random Color',
+                text: 'Be awared that the color that you will get might be a color that you will not be able to see correctly on screen, if it is so than just press the End Game button to go back to this page and choose another color'
             }
         ];
 
         return (
             <section id="choose-color">
                 <h1>CHOOSE COLOR</h1>
-                <ul id="color-chooser">
+                <h2>Choose your ship color :</h2>
+                <ul>
                     {
-                        colors.map(({ colId, colName }) => {
-                            console.log(colId);
+                        colors.map(({ colId, colName, text }) => {
+                            var p;
+
+                            if (text) {
+                              p = <p className="desc">{ text }</p>
+
+                            } else {
+                              p = (
+                                <svg>
+                                    <path fill={colId} d="M 30,10 L 5,0 L 10,7.5 L 0,10 L 10,12.5 L 5,20 Z" />
+                                </svg>
+                              );
+                            }
+
                             return (
                               <li>
                                   <p>{colName}</p>
-                                  <svg>
-                                      <path fill={colId} d="M 30,10 L 5,0 L 10,7.5 L 0,10 L 10,12.5 L 5,20 Z" />
-                                  </svg>
+                                  { p }
                               </li>
                             );
                         })
