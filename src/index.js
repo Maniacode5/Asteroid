3c53689a64272117cc272292e02bbc43de29b05c
I2etr0 <37339397+D4ddyPaul@users.noreply.github.com>
2018-09-28 14:58:43 +0200

    Paul boulet pour l equipe (#19)
    * router
    
    * home
    
    * color choice
    
    * test
    
    * homework
    
    * save work
    
    * plus q1 probleme
    
    * last changes pls met en commun
    
    * remove node modules

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './css/main.css';
 import App from './js/App.jsx';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>, document.getElementById('root'));
 registerServiceWorker();
